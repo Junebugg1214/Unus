01a2b7359464e84096510a3d4f55723a85d4158a
Junebugg1214 <82672745+Junebugg1214@users.noreply.github.com>
2024-10-20 20:20:23 -0400

    commit all

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+const config = {
   darkMode: ["class"],
   content: [
     './pages/**/*.{js,jsx,ts,tsx}',
@@ -69,7 +68,10 @@ const config: Config = {
   plugins: [],
 };
 
-export default config;
+module.exports = config;
+
+
+
 
 
 
